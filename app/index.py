7debc50fa951fd42995098fc5666a55cc2a0becd
Kalyan Dutia <kalyan.dutia@gmail.com>
2022-06-21 15:49:02 +0100

    add methods from main repo common folder

--- app/index.py
+++ app/index.py
@@ -4,7 +4,7 @@ from opensearchpy import OpenSearch, helpers
 from tqdm.auto import tqdm
 import requests
 
-from navigator.core.log import get_logger
+from app.utils import get_logger
 
 logger = get_logger(__name__)
 
