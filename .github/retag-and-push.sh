8ddad94572f534f13da2139dd9934a0c55beae5c
diversemix <diversemix@gmail.com>
2022-11-16 13:35:05 +0000

    update CI scripts

--- .github/retag-and-push.sh
+++ .github/retag-and-push.sh
@@ -1,86 +1,97 @@
 #!/bin/bash
 set -e
 
-if [ "$#" -gt 3 ] || [ "$#" -lt 2 ]; then
+script_folder=$(dirname "${BASH_SOURCE[0]}")
+source $script_folder/funcs.sh
+
+if [ "$#" -ne 2 ]; then
     echo "Pushes a container image to ECR with tags"
     echo
     echo "Usage: $0 project input_tag"
-    echo "Example: $0 container-name 6cd9d7ebad4f16ef7273a7a831d79d5d5caf4164 DEV"
+    echo "Example: $0 container-name 6cd9d7ebad4f16ef7273a7a831d79d5d5caf4164"
     echo "Relies on the following environment variables:"
     echo "- GITHUB_HEAD_REF, GITHUB_REF, GITHUB_SHA (GH Action default)"
     echo "- AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, DOCKER_REGISTRY"
     exit 1
 fi
 
-project="$1"
-image_tag="$2"
-optional_suffix="$3"
-
-if [ "${optional_suffix}" != "" ]; then
-    echo "Setting AWS_ACCESS_KEY_ID to the value in AWS_ACCESS_KEY_ID_${optional_suffix}"
-    echo "Setting AWS_SECRET_ACCESS_KEY to the value in AWS_SECRET_ACCESS_KEY_${optional_suffix}"
-    echo "Setting DOCKER_REGISTRY to the value in DOCKER_REGISTRY_${optional_suffix}"
-    export AWS_ACCESS_KEY_ID=$(printenv "AWS_ACCESS_KEY_ID_${optional_suffix}")
-    export AWS_SECRET_ACCESS_KEY=$(printenv "AWS_SECRET_ACCESS_KEY_${optional_suffix}")
-    export DOCKER_REGISTRY=$(printenv "DOCKER_REGISTRY_${optional_suffix}")
-fi
-
 [ "${AWS_ACCESS_KEY_ID}" == "" ] && (echo "AWS_ACCESS_KEY_ID is not set" ; exit 1)
 [ "${AWS_SECRET_ACCESS_KEY}" == "" ] && (echo "AWS_SECRET_ACCESS_KEY is not set" ; exit 1)
 [ "${DOCKER_REGISTRY}" == "" ] && (echo "DOCKER_REGISTRY is not set" ; exit 1)
 
+project="$1"
+image_tag="$2"
+
 # login
-aws ecr get-login-password --region eu-west-2 | \
+aws ecr get-login-password --region eu-west-1 | \
     docker login --username AWS --password-stdin "${DOCKER_REGISTRY}"
 
-name="$(echo "${DOCKER_REGISTRY}/${project}" | tr -d '\n' | tr -d ' ')"
+name=$(clean_string "${DOCKER_REGISTRY}/${project}")
 input_image="${project}:${image_tag}"
 
-echo "Input:   ${project}:${image_tag}"
-echo "Output:  ${name}"
-echo "GitRef:  ${GITHUB_REF}"
-echo "Branch:  ${GITHUB_REF/refs\/heads\//}"
-echo "Repo Tag ${name}"
+echo "-------------"
+echo "Input       : ${project}:${image_tag}"
+echo "Output      : ${name}"
+echo "GitRef      : ${GITHUB_REF}"
+echo "GitHeadRef  : ${GITHUB_HEAD_REF}"
+echo "Branch      : ${GITHUB_REF/refs\/heads\//}"
+echo "Repo Tag    : ${name}"
+echo "-------------"
 
-timestamp=$(date --utc +%Y%m%d.%H%M.%S%N)
+docker_tag() {
+    echo "Re-tagging $1 -> $2"
+    docker tag $1 $2
+}
+
+timestamp=$(date --utc -Iseconds | cut -c1-19 | tr -c '[0-9]T\n' '-')
 short_sha=${GITHUB_SHA:0:8}
-
-# Always re-run build to make sure we have appropriate tags for extra repos
-docker build -t "${project}" .
 
 if [[ "${GITHUB_REF}" == "refs/heads"* ]]; then
     # push `branch-sha` tagged image
+
+    # NOTE: Looks like the behaviour has changed for GITHHUB_REF
+    # See: https://github.com/semantic-release/env-ci/issues/157
+    # ... branches will no longer be handled here but in the 'else' statement below.
+
     branch="${GITHUB_REF/refs\/heads\//}"
+    echo "Detected Branch: ${branch}"
+
+
+    # Only update latest if on main
     if [[ "${branch}" = "main" ]]; then
-        # push a `version` tag
-        docker tag "${input_image}" "${name}:${branch}-${timestamp}_${short_sha}"
-        docker push "${name}:${branch}-${timestamp}_${short_sha}"
         # push `latest` tag
-        docker tag "${input_image}" "${name}:latest"
+        docker_tag "${input_image}" "${name}:latest"
         docker push "${name}:latest"
+        # Also tag for any versioning that might get done
+        docker_tag "${input_image}" "${name}:${branch}-${timestamp}-${short_sha}"
+        docker push "${name}:${branch}-${timestamp}-${short_sha}"
+        # Also tag for any versioning that might get done
+        docker_tag "${input_image}" "${name}:${branch}-${short_sha}"
+        docker push "${name}:${branch}-${short_sha}"
     fi
-elif [[ "${GITHUB_REF}" =~ refs/tags/v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*) ]]; then
+elif is_tagged_verion ${GITHUB_REF} ; then
     # push `semver` tagged image
     semver="${GITHUB_REF/refs\/tags\/v/}"
-    major="$(echo "${semver}" | cut -d'.' -f1)"
-    minor="$(echo "${semver}" | cut -d'.' -f2)"
-    patch="$(echo "${semver}" | cut -d'.' -f3)"
+    echo "Detected Tag: ${semver}"
+    major=$(get_major "${semver}")
+    minor=$(get_minor "${semver}")
+    patch=$(get_patch "${semver}")
+    maturity=$(get_maturity "${semver}")
+    echo "Detected Version: ${major} . ${minor} . ${patch} [${maturity}]"
 
-    docker tag "${input_image}" "${name}:${major}.${minor}.${patch}"
-    docker tag "${input_image}" "${name}:${major}.${minor}"
-    docker tag "${input_image}" "${name}:${major}"
-    docker push "${name}:${major}.${minor}.${patch}"
-    docker push "${name}:${major}.${minor}"
-    docker push "${name}:${major}"
+    docker_tag "${input_image}" "${name}:${major}.${minor}.${patch}-${maturity}"
+    docker_tag "${input_image}" "${name}:${major}.${minor}-${maturity}"
+    docker_tag "${input_image}" "${name}:${major}-${maturity}"
+    docker push "${name}:${major}.${minor}.${patch}-${maturity}"
+    docker push "${name}:${major}.${minor}-${maturity}"
+    docker push "${name}:${major}-${maturity}"
 else
-    echo "${GITHUB_REF} is neither a branch head or valid semver tag"
+    echo "${GITHUB_REF} is neither a branch head nor valid semver tag"
+    echo "Assuming '${GITHUB_HEAD_REF}' is a branch"
     if [[ -n "${GITHUB_HEAD_REF}" ]]; then
-        branch="${GITHUB_HEAD_REF}"
-        echo "Not yet publishing for branches, so not publishing for '${branch}'."
-        # docker images
-        # docker tag "${input_image}" "${name}:${branch}-${timestamp}-${short_sha}"
-        # docker images
-        # docker push "${name}:${branch}-${short_sha}-${timestamp}"
+        branch="$(echo ${GITHUB_HEAD_REF}| tr -c '[0-9,A-Z,a-z]' '-')"
+        docker_tag "${input_image}" "${name}:${branch}-${timestamp}-${short_sha}"
+        docker push "${name}:${branch}-${timestamp}-${short_sha}"
     else
         echo "No branch found, not a PR so not publishing."
     fi
