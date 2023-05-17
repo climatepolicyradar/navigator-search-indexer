eb3c309341f38c3dad4337e828ade78f7cb52269
NextGenEng <58440325+THOR300@users.noreply.github.com>
2023-05-17 17:16:17 +0100

    Unit tests for filtering text block type function (#52)
    * Finalising unit tests.
    
    * Improving logging so it is SUPER clear when we are removing a text block.
    
    * updating the get_text_blocks function to allow for retrieving invalid text blocks
    
    * Adding tests to validate the get_text_blocks function.
    
    * Updating tests.
    
    ---------
    
    Co-authored-by: Mark <mark@climatepolicyradar.org>

--- src/utils.py
+++ src/utils.py
@@ -7,14 +7,32 @@ logger = logging.getLogger(__name__)
 
 def replace_text_blocks(block: IndexerInput, new_text_blocks: list[TextBlock]):
     """Updates the text blocks in the IndexerInput object."""
-    logger.debug(f"Replacing text blocks in {block.document_id}")
-
     if block.pdf_data is not None:
         block.pdf_data.text_blocks = new_text_blocks
     elif block.html_data is not None:
         block.html_data.text_blocks = new_text_blocks
 
     return block
+
+
+def filter_blocks(indexer_input: IndexerInput, remove_block_types: list[str]) -> list[TextBlock]:
+    """Given an Indexer Input filter the contained TextBlocks and return this as a list of TextBlocks."""
+    filtered_blocks = []
+    for block in indexer_input.get_text_blocks(including_invalid_html=True):
+        if block.type.title() not in remove_block_types:
+            filtered_blocks.append(block)
+        else:
+            logger.info(
+                f"Filtered {block.type} block from {indexer_input.document_id}.",
+                extra={
+                    "props": {
+                        "document_id": indexer_input.document_id,
+                        "block_type": block.type,
+                        "remove_block_types": remove_block_types
+                    }
+                }
+            )
+    return filtered_blocks
 
 
 def filter_on_block_type(inputs: list[IndexerInput], remove_block_types: list[str]) -> list[IndexerInput]:
@@ -27,14 +45,10 @@ def filter_on_block_type(inputs: list[IndexerInput], remove_block_types: list[st
             logger.warning(f"Blocks to filter should be of a known block type, removing {_filter} from the list.")
             remove_block_types.remove(_filter)
 
-    logger.debug(f"Filtering on block types: {remove_block_types}")
     return [
         replace_text_blocks(
             block=_input,
-            new_text_blocks=[
-                block for block in _input.get_text_blocks()
-                if block.type.title() not in remove_block_types
-            ]
+            new_text_blocks=filter_blocks(indexer_input=_input, remove_block_types=remove_block_types)
         )
         for _input in inputs
     ]
