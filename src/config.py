619da452fdc28ae3911c0f160e662cabbff31496
Mark <mark@climatepolicyradar.org>
2023-06-19 11:28:40 +0100

    updating the import id regular expression to match other areas of the pipeline.

--- src/config.py
+++ src/config.py
@@ -7,12 +7,14 @@ import re
 SBERT_MODEL: str = os.getenv("SBERT_MODEL", "msmarco-distilbert-dot-v5")
 INDEX_ENCODER_CACHE_FOLDER: str = os.getenv("INDEX_ENCODER_CACHE_FOLDER", "/models")
 ENCODING_BATCH_SIZE: int = int(os.getenv("ENCODING_BATCH_SIZE", "32"))
+# comma-separated 2-letter ISO codes
 TARGET_LANGUAGES: Set[str] = set(
     os.getenv("TARGET_LANGUAGES", "en").lower().split(",")
-)  # comma-separated 2-letter ISO codes
+)
 ENCODER_SUPPORTED_LANGUAGES: Set[str] = {"en"}
 FILES_TO_PROCESS = os.getenv("FILES_TO_PROCESS")
 BLOCKS_TO_FILTER = os.getenv("BLOCKS_TO_FILTER", "Table,Figure").split(",")
-# FIXME make sure this aligns with the updates to the fanout etc.
-ID_PATTERN = re.compile(r"^[a-zA-Z0-9]+\.[a-zA-Z0-9]+\.[a-zA-Z0-9]+\.[a-zA-Z0-9]+")
+# This matches the ID pattern enforced by the backend, maybe we should share this code?
+_ID_ELEMENT = r"[a-zA-Z0-9]+([-_]?[a-zA-Z0-9]+)*"
+ID_PATTERN = rf"{_ID_ELEMENT}\.{_ID_ELEMENT}\.{_ID_ELEMENT}\.{_ID_ELEMENT}"
 S3_PATTERN = re.compile(r"s3://(?P<bucket>[\w-]+)/(?P<prefix>.+)")
