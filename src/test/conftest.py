5dd6ac13b1151f5143841c8ff477a929f9628fad
Mark <mark@climatepolicyradar.org>
2023-07-03 14:28:35 +0100

    Getting unit tests to pass.

--- src/test/conftest.py
+++ src/test/conftest.py
@@ -1,9 +1,95 @@
 import datetime
 from typing import List
+import json
+import os
+import boto3
+import botocore.client
 import pytest
+from moto import mock_s3
 
 from src.base import IndexerInput, DocumentMetadata, HTMLData
 from src.test.test_utils import get_text_block
+
+
+class S3Client:
+    """Helper class to connect to S3 and perform actions on buckets and documents."""
+
+    def __init__(self, region):
+        self.client = boto3.client(
+            "s3",
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+            config=botocore.client.Config(
+                signature_version="s3v4",
+                region_name=region,
+                connect_timeout=10,
+            ),
+        )
+
+
+@pytest.fixture
+def s3_bucket_and_region() -> dict:
+    return {
+        "bucket": "test-bucket",
+        "region": "eu-west-1",
+    }
+
+
+@pytest.fixture
+def test_file_json() -> dict:
+    return {
+        "document_id": "test_id",
+    }
+
+
+@pytest.fixture
+def test_prefix() -> str:
+    return "test_prefix"
+
+
+@pytest.fixture
+def test_file_key(s3_bucket_and_region, test_prefix) -> str:
+    return f"{s3_bucket_and_region['bucket']}/{test_prefix}/test_id.json"
+
+
+@pytest.fixture
+def pipeline_s3_objects(
+    test_prefix,
+    test_file_json,
+):
+    """
+    Return a dict of s3 objects to be used in the pipeline s3 client fixture.
+
+    This sets up a s3 bucket with the following objects:
+    - A fake document with a document_id of "test_id".
+
+    Thus, we have a document that embeddings can be generated from in s3.
+    """
+    return {
+        f'{test_prefix}/test_id.json': bytes(json.dumps(test_file_json).encode("UTF-8")),
+    }
+
+
+@pytest.fixture
+def pipeline_s3_client(s3_bucket_and_region, pipeline_s3_objects):
+    with mock_s3():
+        s3_client = S3Client(s3_bucket_and_region["region"])
+
+        s3_client.client.create_bucket(
+            Bucket=s3_bucket_and_region["bucket"],
+            CreateBucketConfiguration={
+                "LocationConstraint": s3_bucket_and_region["region"]
+            },
+        )
+
+        for key in pipeline_s3_objects:
+            s3_client.client.put_object(
+                Bucket=s3_bucket_and_region["bucket"],
+                Key=key,
+                Body=pipeline_s3_objects[key],
+            )
+
+        yield s3_client
 
 
 @pytest.fixture
---
+++ src/test/test_s3.py
@@ -0,0 +1,63 @@
+import json
+
+import numpy as np
+
+from src.s3 import validate_s3_pattern, check_file_exists_in_s3, get_s3_keys_with_prefix, s3_object_read_text, \
+    write_json_to_s3, save_ndarray_to_s3_as_npy
+
+
+def test_validate_s3_pattern(test_file_key):
+    """Test that validate_s3_pattern returns the correct bucket, client and key."""
+    bucket, key, s3client = validate_s3_pattern(f"s3://{test_file_key}")
+
+    assert bucket == "test-bucket"
+    assert key == "test_prefix/test_id.json"
+    assert s3client is not None
+
+    try:
+        validate_s3_pattern(f"random_string")
+    except Exception as e:
+        assert "Key does not represent an s3 path: random_string" in str(e)
+
+
+def test_check_file_exists_in_s3(pipeline_s3_client, test_file_key):
+    """Test whether we can check whether a file exists in s3."""
+
+    assert check_file_exists_in_s3(f"s3://{test_file_key}")
+    assert not check_file_exists_in_s3(f"s3://random_bucket/prefix/file.json")
+
+
+def test_get_s3_keys_with_prefix(pipeline_s3_client, s3_bucket_and_region, test_prefix, test_file_key):
+    """Test that we can get a list of keys with a given prefix."""
+    assert get_s3_keys_with_prefix(f"s3://{s3_bucket_and_region['bucket']}/{test_prefix}/") == [f'{test_prefix}/test_id.json']
+
+    try:
+        get_s3_keys_with_prefix(f"random_string")
+    except Exception as e:
+        assert "Prefix does not represent an s3 path: random_string" in str(e)
+
+
+def test_s3_object_read_text(pipeline_s3_client, test_file_key, test_file_json):
+    """Test that we can read text from an s3 object."""
+    assert json.loads(s3_object_read_text(f"s3://{test_file_key}")) == test_file_json
+
+
+def test_write_json_to_s3(pipeline_s3_client, s3_bucket_and_region, test_file_json):
+    """Test that we can write json to an s3 object."""
+    write_json_to_s3(bytes(json.dumps(test_file_json).encode("UTF-8")), f"s3://{s3_bucket_and_region['bucket']}/prefix/test.json")
+    assert json.loads(
+        s3_object_read_text(f"s3://{s3_bucket_and_region['bucket']}/prefix/test.json")
+    ) == test_file_json
+
+
+def test_save_ndarray_to_s3_as_npy(pipeline_s3_client, s3_bucket_and_region):
+    """Test that we can save an ndarray to s3."""
+    save_ndarray_to_s3_as_npy(np.array([1, 2, 3]), f"s3://{s3_bucket_and_region['bucket']}/prefix/test.npy")
+
+    assert check_file_exists_in_s3(f"s3://{s3_bucket_and_region['bucket']}/prefix/test.npy")
+
+    try:
+        save_ndarray_to_s3_as_npy(np.array([1, 2, 3]), "s3://random_bucket/prefix/test.npy")
+    except Exception as e:
+        assert f"Bucket random_bucket does not exist" in str(e)
+
