c411878b8f32583c6b33dddac87e1f69b2099ef7
olaughter <fred@climatepolicyradar.org>
2023-10-19 11:52:06 +0100

    feat: allow vespa secrets to be passed into container
    Co-authored-by: NextGenEng <THOR300@users.noreply.github.com>

--- cli/run.sh
+++ cli/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
 
+if [[ -z ${VESPA_KEY_LOCATION} ]]; then
+    echo "VESPA_KEY_LOCATION not found"
+else
+    mkdir -p $(dirname $VESPA_KEY_LOCATION)
+    echo ${VESPA_PRIVATE_KEY} > ${VESPA_KEY_LOCATION}
+fi
+
 python -m cli.index_data --s3 "${INDEXER_INPUT_PREFIX}"
