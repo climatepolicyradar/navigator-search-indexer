a135d945d343632124f045a3564793a14cca251c
olaughter <fred@climatepolicyradar.org>
2023-10-24 11:44:57 +0100

    fix: env variable typo

--- cli/run.sh
+++ cli/run.sh
@@ -6,7 +6,7 @@ echo Setting up for vespa
 mkdir -p $(dirname $VESPA_KEY_LOCATION)
 echo ${VESPA_PRIVATE_KEY} > ${VESPA_KEY_LOCATION}
 
-mkdir -p $(dirname $VESPA_PUBLIC_CERT_LOCATION)
-echo ${VESPA_PUBLIC_CERT} > ${VESPA_PUBLIC_CERT_LOCATION}
+mkdir -p $(dirname $VESPA_CERT_LOCATION)
+echo ${VESPA_PUBLIC_CERT} > ${VESPA_CERT_LOCATION}
 
 python -m cli.index_data --s3 "${INDEXER_INPUT_PREFIX}" --index-type vespa
