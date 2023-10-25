b17e5e7b5909f2efcf09fb65c4cd10506f36c9ba
olaughter <fred@climatepolicyradar.org>
2023-10-25 17:39:21 +0100

    fix: add extra flags for handling newlines

--- cli/run.sh
+++ cli/run.sh
@@ -7,8 +7,8 @@ mkdir -p $(dirname $VESPA_KEY_LOCATION)
 mkdir -p $(dirname $VESPA_CERT_LOCATION)
 
 # These values where initially encoded from the key and cert with:
-# $ openssl base64 -in <file>
-echo "${VESPA_PRIVATE_KEY}" | openssl base64 -d --out ${VESPA_KEY_LOCATION}
-echo "${VESPA_PUBLIC_CERT}" | openssl base64 -d --out ${VESPA_CERT_LOCATION}
+# $ openssl base64 -a -A -in <file>
+echo "${VESPA_PRIVATE_KEY}" | openssl base64 -d -a -A --out ${VESPA_KEY_LOCATION}
+echo "${VESPA_PUBLIC_CERT}" | openssl base64 -d -a -A --out ${VESPA_CERT_LOCATION}
 
 python -m cli.index_data --s3 "${INDEXER_INPUT_PREFIX}" --index-type vespa
