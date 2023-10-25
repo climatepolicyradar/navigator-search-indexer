0f0f8dc2f2239bd7b9e671b50098d68836727c58
olaughter <51889566+olaughter@users.noreply.github.com>
2023-10-25 17:28:17 +0100

    Use base64 encoded secrets instead (#72)
    * Use base64 encoded secrets instead
    
    We've switched to having writing the certs to pulumi as base64 encoded
    strings to make them easier to work with, we now need to decode them
    
    * doc: add explanation around openssl use

--- cli/run.sh
+++ cli/run.sh
@@ -4,9 +4,11 @@ set -e
 echo Setting up for vespa
 
 mkdir -p $(dirname $VESPA_KEY_LOCATION)
-echo "${VESPA_PRIVATE_KEY}" > ${VESPA_KEY_LOCATION}
-
 mkdir -p $(dirname $VESPA_CERT_LOCATION)
-echo "${VESPA_PUBLIC_CERT}" > ${VESPA_CERT_LOCATION}
+
+# These values where initially encoded from the key and cert with:
+# $ openssl base64 -in <file>
+echo "${VESPA_PRIVATE_KEY}" | openssl base64 -d --out ${VESPA_KEY_LOCATION}
+echo "${VESPA_PUBLIC_CERT}" | openssl base64 -d --out ${VESPA_CERT_LOCATION}
 
 python -m cli.index_data --s3 "${INDEXER_INPUT_PREFIX}" --index-type vespa
