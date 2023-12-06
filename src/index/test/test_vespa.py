142ef30ee439bc9f3d78e57857d0cab92a2612af
Mark <mark@climatepolicyradar.org>
2023-12-06 09:19:23 +0000

    Adding test for document passage matches.

--- src/index/test/test_vespa.py
+++ src/index/test/test_vespa.py
@@ -34,9 +34,12 @@ def test_get_vespa_instance() -> None:
 
 @patch("src.index.vespa_.read_npy_file")
 def test_get_document_generator(mock_read_npy_file):
+    """Assert that the vespa document generator works as expected."""
     mock_read_npy_file.return_value = read_npy_file(
         Path("src/index/test/data/CCLW.executive.10002.4495.npy")
     )
+
+    embedding_dir_as_path = S3Path("s3://path/to/embeddings")
 
     # An array of ParserOutputs, some belonging to the same family.
     tasks = [
@@ -45,18 +48,24 @@ def test_get_document_generator(mock_read_npy_file):
         get_parser_output(document_id=2, family_id=1),
     ]
 
-    embedding_dir_as_path = S3Path("s3://path/to/embeddings")
-
     generator = get_document_generator(tasks, embedding_dir_as_path)
 
     vespa_family_documents = []
-    vespa_document_passages = []
+    vespa_document_passages = {}
     for schema, id, data in generator:
         if schema == FAMILY_DOCUMENT_SCHEMA:
             vespa_family_documents.append(id)
         if schema == DOCUMENT_PASSAGE_SCHEMA:
-            vespa_document_passages.append(id)
+            vespa_document_passages[id] = data
 
     # Check every id is unique and that there's one for each task
     assert len(set(vespa_family_documents)) == len(vespa_family_documents)
     assert len(vespa_family_documents) == len(tasks)
+
+    for id in vespa_document_passages:
+        # A document passage id CCLW.executive.0.0.0 would take the form
+        # 'id:doc_search:family_document::CCLW.executive.0.0'
+        family_doc_ref_id_format = (
+            vespa_document_passages[id]["family_document_ref"].split(":")[-1] + ".0"
+        )
+        assert id == family_doc_ref_id_format
