fab3487a51fa7cfe507c87683a3a7aa682d8e514
olaughter <fred@climatepolicyradar.org>
2024-02-21 12:15:23 +0000

    Consolidate duplicate test
    The generator had two tests associated with it with overlap between them
    this consolidates the test cases into a single test, while also expanding
    the coverage

--- tests/src/test_vespa.py
+++ tests/src/test_vespa.py
@@ -1,8 +1,3 @@
-from cloudpathlib import S3Path
-from pathlib import Path
-from typing import Any, Generator
-from unittest.mock import Mock, patch
-
 from cpr_data_access.parser_models import ParserOutput
 import numpy as np
 import pytest
@@ -20,8 +15,9 @@ from src.index.vespa_ import (
     SEARCH_WEIGHTS_SCHEMA,
     FAMILY_DOCUMENT_SCHEMA,
     DOCUMENT_PASSAGE_SCHEMA,
+    _SCHEMAS_TO_PROCESS,
 )
-from src.utils import read_npy_file
+
 from tests.conftest import get_parser_output, FIXTURE_DIR
 
 
@@ -92,129 +88,82 @@ def test_determine_stray_ids():
     assert sorted(stray_ids) == ["C.1.4", "C.1.5"]
 
 
-@patch("src.index.vespa_.read_npy_file")
-def test_get_document_generator(mock_read_npy_file, test_vespa):
+def test_get_document_generator(test_vespa):
     """Assert that the vespa document generator works as expected."""
-    mock_read_npy_file.return_value = read_npy_file(
-        FIXTURE_DIR / "s3_files" / "CCLW.executive.10002.4495.npy"
-    )
-
-    embedding_dir_as_path = Path("s3://path/to/embeddings")
-
-    # An array of ParserOutputs, some belonging to the same family.
+    embedding_dir_as_path = FIXTURE_DIR / "s3_files"
     paths = [
-        FIXTURE_DIR / "s3_files" / "CCLW.executive.10002.4495.json",
-        FIXTURE_DIR / "s3_files" / "CCLW.executive.10014.4470.json",
+        embedding_dir_as_path / "CCLW.executive.10002.4495.json",
+        embedding_dir_as_path / "CCLW.executive.10014.4470.json",
+        embedding_dir_as_path / "CCLW.document.i00000004.n0000.json",
     ]
+
+    fixture_doc_ids = []
+    fixture_text_blocks = []
+    for path in paths:
+        fixture_content = ParserOutput.model_validate_json(path.read_text())
+        fixture_doc_ids.append(fixture_content.document_id)
+        fixture_text_blocks.extend(fixture_content.text_blocks)
 
     generator = get_document_generator(test_vespa, paths, embedding_dir_as_path)
 
-    vespa_family_document_ids = []
-    vespa_document_passage_fam_refs = []
-    for schema, id, data in generator:
-        if schema == FAMILY_DOCUMENT_SCHEMA:
-            vespa_family_document_ids.append(id)
-        if schema == DOCUMENT_PASSAGE_SCHEMA:
-            vespa_document_passage_fam_refs.append(data["family_document_ref"])
+    EXPECTED_DOCUMENTS = 3
+    EXPECTED_PASSAGES = 1978
+    assert EXPECTED_DOCUMENTS == len(paths)
+    assert EXPECTED_PASSAGES == len(fixture_text_blocks)
 
-    # Check every family document id is unique and that there's one for each task
-    assert len(set(vespa_family_document_ids)) == len(vespa_family_document_ids)
-    assert len(vespa_family_document_ids) == len(paths)
 
-    # Check that every family document is referenced by one passage
-    # (this is as we had one text block for each family document)
-    assert len(vespa_family_document_ids) == len(set(vespa_document_passage_fam_refs))
-    for ref in vespa_document_passage_fam_refs:
+    schemas = []
+    ids = []
+    document_passage_ids = []
+    family_document_refs = []
+    for schema, doc_id, data in generator:
+        schemas.append(schema)
+        ids.append(doc_id)
+
+        assert data
+        assert isinstance(data, dict)
+
+        if schema == SEARCH_WEIGHTS_SCHEMA:
+            VespaSearchWeights.model_validate(data)
+        elif schema == DOCUMENT_PASSAGE_SCHEMA:
+            VespaDocumentPassage.model_validate(data)
+            family_document_refs.append(data["family_document_ref"])
+            document_passage_ids.append(doc_id)
+        elif schema == FAMILY_DOCUMENT_SCHEMA:
+            VespaFamilyDocument.model_validate(data)
+        else:
+            pytest.exit(f"Unexpected schema: {schema}")
+
+
+    # Test schemas
+    assert len(set(schemas)) == len(_SCHEMAS_TO_PROCESS)
+    for schema in _SCHEMAS_TO_PROCESS:
+        assert schema in schemas
+    assert schemas.count("search_weights") == 1
+    assert schemas.count("family_document") == EXPECTED_DOCUMENTS
+    assert schemas.count("document_passage") == EXPECTED_PASSAGES
+
+    # Test ids
+    assert len(set(ids)) == len(ids)
+    assert "default_weights" in ids
+
+    # Documents belong to the specific families
+    # We expect this to be 2 families as only two fixture docs have passages
+    assert len(family_document_refs) == EXPECTED_PASSAGES
+    assert len(set(family_document_refs)) == 2
+
+    for doc_id in fixture_doc_ids:
+        assert doc_id in ids
+        assert doc_id not in document_passage_ids
+
+    # Check every passage references a family document
+    for ref in set(family_document_refs):
         # A document passage id CCLW.executive.0.0.0 would take the form
         # 'id:doc_search:family_document::CCLW.executive.0.0'
-        ref_id_format = ref.split(":")[-1]
-        assert ref_id_format in vespa_family_document_ids
+        id_parts = ref.split("::")
+        family_schema = id_parts[0].split(":")[-1]
+        family_id = id_parts[-1]
 
-
-@patch("src.index.vespa_.read_npy_file")
-def test_get_document_generator_(
-    mock_np_load: Mock,
-    test_vespa,
-    test_document_data: tuple[ParserOutput, Any],
-    embeddings_dir_as_path: S3Path,
-) -> None:
-    """Test that the get_document_generator correctly yields data."""
-    parser_output_path, embeddings = test_document_data
-    
-    parser_output = ParserOutput.model_validate_json(parser_output_path.read_text())
-    # Test that the pdf data is valid to test against
-    assert parser_output.pdf_data is not None
-
-    parser_output_tb_pages = {
-        block.page_number for block in parser_output.pdf_data.text_blocks
-    }
-    parser_output_md_pages = {
-        page.page_number for page in parser_output.pdf_data.page_metadata
-    }
-
-    # All text block pages should exist in the page metadata object. Not all metadata
-    # object pages should have a text block page as we may not have retrieved text for
-    # every page.
-    for text_block_page in parser_output_tb_pages:
-        assert text_block_page in parser_output_md_pages
-
-    mock_np_load.return_value = embeddings
-
-    document_generator = get_document_generator(
-        vespa=test_vespa, paths=[parser_output_path], embedding_dir_as_path=embeddings_dir_as_path
-    )
-
-    # Only loading one document so we know the order of schemas that should be
-    # returned. This is helpful as we can strongly assert the yield structure.
-    assert isinstance(document_generator, Generator)
-
-    document = next(document_generator, None)
-    assert document is not None
-
-    schema, document_id, data = document
-    assert schema == SEARCH_WEIGHTS_SCHEMA
-    # TODO Can we get a list of options for this?
-    assert document_id == "default_weights"
-    assert isinstance(data, dict)
-    VespaSearchWeights.model_validate(data)
-
-    document = next(document_generator, None)
-    assert document is not None
-
-    schema, document_id, data = document
-    assert schema == FAMILY_DOCUMENT_SCHEMA
-    assert document_id == parser_output.document_metadata.import_id
-    assert isinstance(data, dict)
-    VespaFamilyDocument.model_validate(data)
-
-    def validate_passages_and_collect_page_numbers(
-        doc_gen: Generator, pages: set, counter: int
-    ) -> set[int]:
-        """
-        A recursive function to iterate through the generator.
-
-        The purpose is to validate the document passage content and collect the page
-        numbers into set for testing.
-        """
-        document = next(doc_gen, None)
-        if document is None:
-            return pages
-        assert document is not None
-
-        schema, document_id, data = document  # type: ignore
-        assert schema == DOCUMENT_PASSAGE_SCHEMA
-        document_passage = VespaDocumentPassage.model_validate(data)
-
-        id_page = document_passage.text_block_id.split("_")[1]
-
-        assert document_id == parser_output.document_id + f".{counter}"
-        assert document_passage.text_block_page == int(id_page)
-        pages.add(document_passage.text_block_page)
-
-        return validate_passages_and_collect_page_numbers(doc_gen, pages, counter + 1)
-
-    document_pages = validate_passages_and_collect_page_numbers(
-        document_generator, set(), 0
-    )
-
-    assert document_pages == parser_output_tb_pages
+        assert family_schema == FAMILY_DOCUMENT_SCHEMA
+        assert family_id in ids
+        assert len(family_id.split(".")) == 4
